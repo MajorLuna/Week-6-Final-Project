89ff7c5e04b10d7dba73cb4f18c7a012faacfff2
Jonathan Luna <123226786+MajorLuna@users.noreply.github.com>
2023-03-11 23:01:43 -0600

    Add files via upload

--- Week 6.js
+++ Week 6.js
@@ -1,6 +1,8 @@
 class Player{
-    constructor(){
+    constructor(name){
         this.playerHand=[];
+        this.score= 0;
+        this.name= name;
     }
 }
 
@@ -17,79 +19,76 @@ class Card{
 }
 
 class Deck{
-    constructor(){; 
+    constructor(){ 
         this.deck =[]
-        this.suit =["Spades", "Hearts", "Diamonds", "Clubs"];
-        this.value =[ '1', '2', '3', '4', '5', '6', '7','8', '9', '10', '11', '12', '13']
-        this.rank =['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+        let suit =["Spades", "Hearts", "Diamonds", "Clubs"];
+        let value =[ '1', '2', '3', '4', '5', '6', '7','8', '9', '10', '11', '12', '13']
+        let rank =['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
         // console.log("creating cards...");   
-
-
-// Change here
-    createCards();{
-        console.log("creating cards...");
-    for (let i = 0; i < this.suit.length; i++); {
-        console.log(i,"suit","", this.suit[i]);
-    }
-
+        for (let i = 0; i < suit.length; i++) {
+            // console.log(i,"suit","", this.suit[i]);
     
-    for (let j = 0; j < this.rank.length; j++); {
-            console.log(i,'rank',"", this.rank[j]);
-            
-            let suits = this.suit[i];
-            let value = this.value[v];
-            let rank = this.rank[j];
-            this.deck.push(newCard(suit, value, rank));   
-    
-}
-    console.log(this.deck);
-
-    dealShuffledDeck();{
-        console.log(ShuffledCards);
-        const ShuffledCards = this.deck.slice();
-        const DeckShuffled = {}
-    while (ShuffledCards.length > 0) {
-        const randomindex = Math.floor(Math.random() * ShuffledCards.length);
-        DeckShuffled.push(ShuffledCards[randomindex]);
-        console.log(DeckShuffled);
         
-    dealCards();{
-        let newDeck = newDeck();
-        newDeck.createcards();
-        newDeck.shuffle();
-        newDeck.dealCards();
-
-        let playerOne = Player();
-        let playerTwo = Player();
-    for (let i = 0; 1 < ShuffledCards.length; i++){
-        if(i<26){
-            playerOne.playerHand.push(DeckShuffled[i]);
-        } else{
-            playerTwo.playerHand.push(DeckShuffled[i]);
+        for (let j = 0; j < rank.length; j++) {
+                // console.log(i,'rank',"", this.rank[j]);
+                
+                // let suit = this.suit[i];
+                // let value = this.value[j];
+                // let rank = this.rank[j];
+                this.deck.push(new Card(suit[i], j, rank[j])); 
+        }  
+    }    
+}  
+    shuffle(){
+        this.deck.length
+        for(let i = this.deck.length -1; i >= 0; i--){
+            const newCard = Math.floor(Math.random()* this.deck.length);
+            const oldCard = this.deck[newCard];
+            this.deck[newCard]= this.deck[i];
+            this.deck[i] = oldCard;
         }
     }
+}
 
-        console.log(playerOne.playerHand);
-        console.log(playerTwo.playerHand);
-        console.log(playerOne.playerHand[2].value);
+let newDeck = new Deck();
+newDeck.shuffle();
+console.log(newDeck);
+           
+    class DealCards {
+        constructor(){
+         let newDeck = new Deck();
+        newDeck.createcards();
+        newDeck.shuffle();
+     let playerOne = ('Aura');
+        let playerTwo = ('Summer');
+    for (let i = 0; i < newDeck.length; i++){
+        if(i<26){
+            playerOne.playerHand.push(newDeck[i])
+        } else{
+            playerTwo.playerHand.push(newDeck[i])
+        }
+    
+        console.log(playerOne.playerHand)
+        console.log(playerTwo.playerHand)
        
-        let playerOneResult = 0;
+
+         let playerOneResult = 0;
         let playerTwoResult = 0;
    
-        for( let i = 0; i < playerOne.playerHand.length; i++){
-            if(playerOne.playerHand[i].value > playerTwo.playerHand[i].value){playerOneResult += 1;}
-        else if(playerOne.playerHand[i].value < playerTwo.playerHand[i].value){playerTwoResult += 1;}
+        for( let i = 0; i < Player.playerHand.length; i++){
+            if(Player.playerHand[i].value > Player.playerHand[i].value){playerOneResult += 1;}
+        else if(Player.playerHand[i].value < Player.playerHand[i].value){playerTwoResult += 1;}
          else {
             playerOneResult += 0;
             playerTwoResult += 0;
-
+         }
         }
-        console.log(playerOne, $(playerOneResult));
-            console.log(playerTwo,$(playerTwoResult));
-    }
-        if(playerOneResult > playerTwoResult){
+         console.log(Player, $(playerOneResult));
+            console.log(Player,$(playerTwoResult));
+
+        while (playerOneResult > playerTwoResult){
             console.log("Player One Wins !!!");
-        }else if(playerTwoResult > playerOneResult){
+        }  if(playerTwoResult > playerOneResult){
             console.log("Player Two Wins !!!");
         } else{
             console.log("It's a TIE !!!");
@@ -97,6 +96,3 @@ class Deck{
     }
 }
     }
-}
-    }
-}
